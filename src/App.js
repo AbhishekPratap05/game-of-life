e7351adc410042b6d26bfd556fa8cd4b146a5d3d
Abhishek Pratap <35282546+AbhishekPratap05@users.noreply.github.com>
2021-04-17 23:56:50 +0530

    initial commit, created grid ✅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const rows = 40;
+const cols = 40;
+
+const App = () => {
+
+  const [grid, setGrid] = useState(Array.from(Array(rows), () => Array.from(Array(cols), () => 0))) //setting up 2d array of 50*50
+
+  console.log(grid)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'grid', gridTemplateColumns: `repeat(${cols},20px)` }}>
+      {grid.map((gridRow, i) => gridRow.map((gridItem, j) => {
+        return (
+          <div
+            key={`${i}-${j}`}
+            style={{ width: '20px', height: '20px', border: '1px solid black', backgroundColor: gridItem ? 'teal' : undefined }}
+          >
+          </div>
+        )
+      }))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
