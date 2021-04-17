84960788054269bc35526b664a795d950d9e5f04
Abhishek Pratap <35282546+AbhishekPratap05@users.noreply.github.com>
2021-04-18 01:18:03 +0530

    added simulation funciton with rules

--- src/App.js
+++ src/App.js
@@ -1,20 +1,75 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { produce } from 'immer';
 
-const rows = 40;
-const cols = 40;
+const numRows = 40;
+const numCols = 40;
+
+const neighbours = [
+  [-1, -1], //NW
+  [-1, 0],  //N
+  [-1, 1],  //NE
+  [0, -1],  //W
+  [0, 1], //E
+  [1, -1],  //SW
+  [1, 0], //S
+  [1, 1], //SW //[0,0] is not there as its the cell itself and we want neighbours
+]
 
 const App = () => {
 
-  const [grid, setGrid] = useState(Array.from(Array(rows), () => Array.from(Array(cols), () => 0))) //setting up 2d array of 50*50
+  const [grid, setGrid] = useState(Array.from(Array(numRows), () => Array.from(Array(numCols), () => 0))) //setting up 2d array of 50*50
   const [running, setRunning] = useState(false);
+  const runningRef = useRef(running)
+
+  runningRef.current = running;
+
+  const runSimulation = useCallback(
+    () => {
+      if (!runningRef.current) {
+        return
+      }
+      console.log("called")
+      setGrid((currGrid) => {
+        return produce(currGrid, gridCopy => {
+          for (let i = 0; i < numRows; i++) {
+            for (let j = 0; j < numCols; j++) {
+              let currentCellNeighbours = 0;
+              neighbours.forEach(([x, y]) => {
+                const newI = i + x;
+                const newJ = j + y;
+                if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) { //checking bounds
+                  currentCellNeighbours += currGrid[newI][newJ] //count the number of neighbours it has.
+                }
+              })
+              if (currentCellNeighbours < 2 || currentCellNeighbours > 3) {
+                // Any live cell with fewer than two live neighbours dies
+                // Any live cell with more than three live neighbours dies
+                gridCopy[i][j] = 0;
+                //Any live cell with two or three live neighbours lives on to the next generation
+                //Nothing as to be done for the above rule
+              } else if (currGrid[i][j] === 0 && currentCellNeighbours === 3) {
+                //Any dead cell with exactly three live neighbours becomes a live cell
+                gridCopy[i][j] = 1;
+              }
+            }
+          }
+        })
+      })
+      setTimeout(runSimulation, 100)
+    }, []);
 
 
-
-  console.log(grid)
   return (
     <>
-      <div style={{ display: 'grid', gridTemplateColumns: `repeat(${cols},20px)` }}>
+      <button onClick={() => {
+        setRunning(!running);
+        if (!running) {
+          runningRef.current = true;
+          runSimulation();
+        }
+      }}>{running ? 'stop' : 'start'}</button>
+
+      <div style={{ display: 'grid', gridTemplateColumns: `repeat(${numCols},20px)` }}>
         {grid.map((gridRow, i) => gridRow.map((gridItem, j) => {
           return (
             <div
