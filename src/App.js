dd83e91b0fa2941e92757797d909fd2f31f17b39
Abhishek Pratap <35282546+AbhishekPratap05@users.noreply.github.com>
2021-04-18 12:08:29 +0530

    added range for random and box input for grid size

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useCallback, useRef, useState } from 'react';
 import { produce } from 'immer';
 
-const numRows = 40;
-const numCols = 40;
-
 //neighbours of current cell
 const neighbours = [
   [-1, -1], //NW
@@ -16,16 +13,20 @@ const neighbours = [
   [1, 1], //SW //[0,0] is not there as its the cell itself and we want neighbours
 ];
 
-//setting up 2d array of numRows*numCols with all dead cell
-const generateEmptyGrid = () => Array.from(Array(numRows), () => Array.from(Array(numCols), () => 0));
-
-//setting up 2d array of numRows*numCols with random alive and dead cell
-const generateRandomFilledGrid = () => Array.from(Array(numRows), () => Array.from(Array(numCols), () => Math.random() > 0.7 ? 1 : 0));
-
 const App = () => {
 
+
+  //setting up 2d array of numRows*numCols with all dead cell
+  const generateEmptyGrid = () => Array.from(Array(gridSize), () => Array.from(Array(gridSize), () => 0));
+
+  //setting up 2d array of numRows*numCols with random alive and dead cell
+  const generateRandomFilledGrid = (value) => Array.from(Array(gridSize), () => Array.from(Array(gridSize), () => Math.random() > value ? 1 : 0));
+
+
+  const [gridSize, setGridSize] = useState(40)
   const [grid, setGrid] = useState(() => generateEmptyGrid());
   const [running, setRunning] = useState(false);
+  const [randomRange, setRandomRange] = useState(0.7)
   const runningRef = useRef(running);
 
   runningRef.current = running;
@@ -38,13 +39,13 @@ const App = () => {
       console.log("called")
       setGrid((currGrid) => {
         return produce(currGrid, gridCopy => {
-          for (let i = 0; i < numRows; i++) {
-            for (let j = 0; j < numCols; j++) {
+          for (let i = 0; i < gridSize; i++) {
+            for (let j = 0; j < gridSize; j++) {
               let currentCellNeighbours = 0;
               neighbours.forEach(([x, y]) => {
                 const newI = i + x;
                 const newJ = j + y;
-                if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) { //checking bounds
+                if (newI >= 0 && newI < gridSize && newJ >= 0 && newJ < gridSize) { //checking bounds
                   currentCellNeighbours += currGrid[newI][newJ]; //count the number of neighbours it has.
                 }
               })
@@ -63,29 +64,65 @@ const App = () => {
         })
       })
       setTimeout(runSimulation, 100);
-    }, []);
+    }, [gridSize]);
 
 
   return (
     <>
-      <button onClick={() => {
-        setRunning(!running);
-        if (!running) {
-          runningRef.current = true;
-          runSimulation();
-        }
-      }}>{running ? 'stop' : 'start'}</button>
+      <h2>Conway's Game of Life</h2>
+      <p>{gridSize}</p><p>{running}</p>
+      <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', gap: '10px' }}>
+        <button onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}>{running ? 'stop' : 'start'}</button>
 
-      <button onClick={() => {
-        setRunning(false);
-        setGrid(generateEmptyGrid());
-      }}>clear</button>
+        <button onClick={() => {
+          setRunning(false);
+          setGrid(generateEmptyGrid());
+        }}>clear</button>
 
-      <button
-        onClick={() => {
-          setGrid(generateRandomFilledGrid());
-        }}>random</button>
-      <div style={{ display: 'grid', gridTemplateColumns: `repeat(${numCols},20px)` }}>
+
+        <div>
+          <button
+            onClick={() => {
+              setGrid(generateRandomFilledGrid(randomRange));
+            }}>random</button>
+
+          <input
+            type='range'
+            id='randomRange'
+            min={0}
+            max={1}
+            step={0.1}
+            value={randomRange}
+            onChange={(e) => {
+              setRunning(false);
+              const newValue = parseFloat(e.target.value);
+              setRandomRange(newValue);
+              setGrid(generateRandomFilledGrid(newValue));
+            }} />{randomRange * 100}
+        </div>
+        <div>
+          <input
+            type='number'
+            id='gridRange'
+            min={10}
+            max={100}
+            step={10}
+            placeholder={'number of rows and columns'}
+            value={gridSize}
+            onChange={(e) => {
+              setRunning(false);
+              setGridSize(parseInt(e.target.value));
+              setTimeout(setGrid(generateEmptyGrid()), 100);
+            }} />
+        </div>
+      </div>
+      <div style={{ display: 'grid', gridTemplateColumns: `repeat(${gridSize},20px)` }}>
         {grid.map((gridRow, i) => gridRow.map((gridItem, j) => {
           return (
             <div
